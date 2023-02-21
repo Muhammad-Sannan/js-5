1f829473c4dc5782786942dbb99291777eff80a4
Muhammad Sannan <113182285+Muhammad-Sannan@users.noreply.github.com>
2023-02-21 21:11:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+/*var str = prompt("Enter a word : ");
+var splitStr = str.split().reverse().join();
+
+if(str === splitStr){
+    document.write("its a palindrome ");
+}
+else{
+    document.write("not a palindrome");
+}*/
+
+
+
+var strr = prompt(" Enter a number :")
+for(i = strr.length -1 ; i>= 0 ; i--){
+    console.log(strr[i])
+}
